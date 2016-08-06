b538fbe03afa6b9557ddfbceced923a197084a17
Barbosik <barbos4agar@gmail.com>
2016-08-06 02:09:24 +0200

    update headers

--- BinaryReader.js
+++ BinaryReader.js
@@ -3,8 +3,19 @@
  * Simple BinaryReader is a minimal tool to read binary stream.
  * Useful for binary deserialization.
  *
- * Copyright (c) 2016 Barbosik https://github.com/Barbosik
- * License: Apache License, Version 2.0
+ * Copyright (c) 2016 Barbosik
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 function BinaryReader(buffer) {
@@ -107,12 +118,12 @@ BinaryReader.prototype.readStringZeroUtf8 = function () {
 BinaryReader.prototype.readStringZeroUnicode = function () {
     var length = 0;
     var terminatorLength = ((this._buffer.length - this._offset) & 1) != 0 ? 1 : 0;
-    for (var i = this._offset; i+1 < this._buffer.length; i+=2) {
+    for (var i = this._offset; i + 1 < this._buffer.length; i += 2) {
         if (this._buffer.readUInt16LE(i) == 0) {
             terminatorLength = 2;
             break;
         }
-        length+=2;
+        length += 2;
     }
     var value = this.readStringUnicode(length);
     this._offset += terminatorLength;
